4e6d5544bd6dda5e30ee422bd883f14421491981
Tomás Bernal <tbernalz@eafit.edu.co>
2025-03-15 10:24:19 -0500

    :tada: First commit

---
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -0,0 +1,13 @@
+package co.edu.eafit.protobootapp;
+
+//This is a simple calculator class and I-m editing it
+public class Calculadora {
+
+    public Double cuadrado(Double numero) {
+        return numero * numero;
+    }
+    
+    public Double cubo(Double numero) {
+        return numero * numero * numero;
+    }
+}
